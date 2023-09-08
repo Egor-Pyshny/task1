d738e62e53820f09e4f6862a2b88ecb5a1eb5336
Egor251 <egorpyshny@gmail.com>
2023-09-08 17:52:12 +0300

    Revert "Revert "-""
    This reverts commit 716c10d8ad8ecdc60adaf293bf6373e3457dec8b.

---
+++ task1/channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace task1
+{
+    [Serializable]
+    [XmlRoot("channel")]
+    public class channel
+    {
+        [XmlElement("item")]
+        public List<item> items { get; set; }
+    }
+}
--- task1/item.cs
+++ task1/item.cs
@@ -1,25 +1,42 @@
 ﻿using Ganss.Excel;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace task1
 {
+    [Serializable]
     public class item
     {
         [Column("title")]
+
         public string title { get; set; }
+
         [Column("link")]
+        
         public string link { get; set; }
+
         [Column("description")]
+      
         public string description { get; set; }
+
         [Column("category")]
+        
         public string category { get; set; }
+
         [Column("pubDate")]
+        
         public string pubDate { get; set; }
 
-
+        public static explicit operator Dictionary<string, string>(item v)
+        {
+            return new Dictionary<string, string>{
+                { "title", v.title },
+                { "link", v.link },
+                { "description", v.description },
+                { "category", v.category },
+                { "pubDate", v.pubDate },
+            };
+        }
     }
 }
