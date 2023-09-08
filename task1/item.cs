716c10d8ad8ecdc60adaf293bf6373e3457dec8b
Egor251 <egorpyshny@gmail.com>
2023-09-08 17:51:38 +0300

    Revert "-"
    This reverts commit 7c21f174d226078adcf3b9479141d061ac01040d.

--- task1/item.cs
+++ task1/item.cs
@@ -1,42 +1,25 @@
 ﻿using Ganss.Excel;
 using System;
 using System.Collections.Generic;
-using System.Xml.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace task1
 {
-    [Serializable]
     public class item
     {
         [Column("title")]
-
         public string title { get; set; }
-
         [Column("link")]
-        
         public string link { get; set; }
-
         [Column("description")]
-      
         public string description { get; set; }
-
         [Column("category")]
-        
         public string category { get; set; }
-
         [Column("pubDate")]
-        
         public string pubDate { get; set; }
 
-        public static explicit operator Dictionary<string, string>(item v)
-        {
-            return new Dictionary<string, string>{
-                { "title", v.title },
-                { "link", v.link },
-                { "description", v.description },
-                { "category", v.category },
-                { "pubDate", v.pubDate },
-            };
-        }
+
     }
 }
